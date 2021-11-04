739bd5bae6e007760aca8082a4518b1be6632863
sdarwin <samuel.d.darwin@gmail.com>
2021-11-04 19:20:19 +0000

    test

--- generate_pages.py
+++ generate_pages.py
@@ -14,7 +14,7 @@ generate_pages.py creates the files in question: index.html and libs/libraries.h
 import sys
 import os
 import glob
-import jinja2
+from jinja2 import Environment, BaseLoader
 import json
 import pprint
 from collections import defaultdict
@@ -46,13 +46,10 @@ boostlibrarycategories["String"]["title"] = "String and text processing"
 boostlibrarycategories["System"]["title"] = "System"
 boostlibrarycategories["Workarounds"]["title"] = "Broken compiler workarounds"
 
-boostlibrariestoskip=["libs/headers","libs/numeric"]
-boostlibrariestoadd=["libs/numeric/conversion","libs/numeric/interval","libs/numeric/odeint","libs/numeric/ublas"]
+boostlibrariestoskip=["libs/numeric"]
+boostlibrariestoadd=["libs/numeric/conversion", "libs/numeric/interval", "libs/numeric/odeint", "libs/numeric/ublas"]
 
 def generatehtmlpages():
-    for sourcefile in ["index.html","libs/libraries.htm"]
-    with open(sourcefile, 'r',  encoding="utf-8") as file:
-        file_contents = file.read()
 
     # Ingest all boost library metadata:
     allmetadata={}
@@ -60,23 +57,51 @@ def generatehtmlpages():
     all_libraries.extend(boostlibrariestoadd)
 
     for directoryname in glob.iglob('libs/*', recursive=False):
-        if os.path.isdir(directoryname) and directoryname not in boostlibrariestoskip: # filter dirs
+        if os.path.isdir(directoryname) and (directoryname not in boostlibrariestoskip) and os.path.isfile(directoryname + "/meta/libraries.json"): # filter dirs
             all_libraries.append(directoryname)
 
     for directoryname in all_libraries:
+        librarypath=re.sub(r'^libs/','',directoryname))
         with open(directoryname + "/meta/libraries.json", 'r',  encoding="utf-8") as file:
             file_contents = file.read()
             data = json.loads(file_contents)
             if type(data) is dict:
                 key=data["key"]
                 allmetadata[key]=data
+                allmetadata[key]["librarypath"]=librarypath
             if type(data) is list:
                 for item in data:
                     key=item["key"]
                     allmetadata[key]=item
-    pprint.pprint(allmetadata)
+                    allmetadata[key]["librarypath"]=librarypath
+    # pprint.pprint(allmetadata)
+
+    # fix documentation
+    for item in allmetadata:
+        if 
+
+    # for sourcefile in ["index.html", "libs/libraries.htm"]:
+    for sourcefile in ["libs/libraries.htm"]:
+        with open(sourcefile, 'r',  encoding="utf-8") as file:
+            file_contents = file.read()
+        file_contents = file_contents.replace('{{#categorized}}\n','{% for key, value in boostlibrarycategories.items() %}{% set name = key %}{% set title = value["title"] %}' ) 
+        file_contents = file_contents.replace('{{/categorized}}\n','{% endfor %}')
+        file_contents = file_contents.replace('{{#alphabetic}}\n','{% for key, value in allmetadata.items() %}{% set name = value["name"] %}{% set authors = value["author"] %}')
+        file_contents = file_contents.replace('{{/alphabetic}}\n','{% endfor %}')
+        file_contents = file_contents.replace('{{#libraries}}\n','{% for key, value in allmetadata.items() %}{% set name = value["name"] %}{% set authors = value["author"] %}')
+        file_contents = file_contents.replace('{{/libraries}}\n','{% endfor %}')
+        file_contents = file_contents.replace('{{#authors}}','')
+        file_contents = file_contents.replace('{{/authors}}','')
+        string="""{{! This is a template for the library list. See the generated file at:
+    http://www.boost.org/doc/libs/develop/libs/libraries.htm
+}}
+"""
+        file_contents = file_contents.replace(string,'')
+        # print(file_contents)
+        rtemplate = Environment(loader=BaseLoader).from_string(file_contents)
+        data = rtemplate.render(allmetadata=allmetadata,boostlibrarycategories=boostlibrarycategories)
+        print(data)
 
 if __name__ == "__main__":
     generatehtmlpages()
-
 
